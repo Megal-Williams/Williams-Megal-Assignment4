b986352c6809ac87e13b4d07c8666109d725597e
megalwilliams <megalwilliams@fullsail.edu>
2012-12-13 18:02:42 -0500

    another great induction to the repo

--- Williams_Megal_Assignment4.js
+++ Williams_Megal_Assignment4.js
@@ -50,47 +50,6 @@ var StringLibrary = function () {
 	};
 };
 
-// // Number Library
-var NumberLibrary = function () {
-	// Change a number to use a specific amount of decimal places
-	var formatDecimal = function (num,afterDecimal) {
-        return Number(num.toFixed(afterDecimal));
-    };
-    // Checks to see is the number above or below a number is within a certain percent
-	var fuzzyNum = function (num,compareNum,percent) {
-		var percentage = (num/compareNum) * 100;
-		if ((num >= compareNum && percentage >= percent) || (num < compareNum && percentage < percent)) {
-			return false;
-		} else {
-			return true;
-		};
-	};
-	// Input two dates using new Date(yyyy,mm,dd)
-	// Output format [Days,Hours,Minutes,Seconds] conversion for each at specific index shown
-    var timeBtDates = function (date1,date2) {
-		var results = [];
-		var difference = (date1 > date2) ? date1.getTime() - date2.getTime() : date2.getTime() - date1.getTime();
-		results[3] = difference / 1000;
-		results[2] = results[3] / 60;
-		results[1] = results[2] / 60;
-		results[0] = results[1] / 24;
-		return results;
-
-    };
-    // Changes a string "x" to an integer value x
-    var strToNum = function (num) {
-		return Number(num);
-    };
-
-	return {
-		"formatDecimal" : formatDecimal,
-		"fuzzyNum" : fuzzyNum,
-		"timeBtDates" : timeBtDates,
-		"strToNum" : strToNum
-
-	};
-};
-
 // Array Library
 var ArrayLibrary = function () {
 	// Smallest value in array greater than a given number
@@ -106,20 +65,7 @@ var ArrayLibrary = function () {
 			return null;
 		};
 	};
-	// Largest value in array less than a given number *My own addition to the list
-	// null is returned if the number is not within the range of numbers in the array
-	var lrgValLNumInArray = function (array,num) {
-		array.sort(function(a,b){return a-b;});
-		if (num > array[0] && num <= array[array.length-1]) {
-			array.push(num);
-			array.sort(function(a,b){return a-b;});
-			var result = array[array.indexOf(num) - 1];
-			return result;
-		} else {
-			return null;
-		};
-	};
-	// Add up and total only numbers in an array
+// Add up and total only numbers in an array
 	// edited to not use !isNaN because a string number ex. "10" would return as a number not a string
 	var totalValNumInArray = function (array) {
 		var total = 0;
@@ -154,11 +100,12 @@ var ArrayLibrary = function () {
 		"dupInArray" : dupInArray
 	};
 };
-// Test Area
 
-// Loop speed testing
-// using jsperf.com to test speed
-// http://jsperf.com/fors-vs-while/10
+/* Test Area
+
+Loop speed testing
+using jsperf.com to test speed
+http://jsperf.com/fors-vs-while/10 */
 
 // String Tests
 console.log("String Tests");
@@ -168,19 +115,6 @@ console.log(stringLib.isEmailAddr("test@test.com"));
 console.log(stringLib.isUrl("http://test.com"));
 console.log(stringLib.splitStrUpper("Megal Williams testing test"));
 console.log(stringLib.swapSeparator("a,b,c","/"));
-console.log(" ");
-
-// Number Tests
-console.log("Number Tests");
-var numberLib = NumberLibrary();
-console.log(numberLib.formatDecimal(2.116,2));
-console.log(numberLib.fuzzyNum(10,5,50));
-console.log(numberLib.fuzzyNum(5,10,50));
-var date1 = new Date(2012,9,22);
-var date2 = new Date(2012,5,19);
-var timeConversion = numberLib.timeBtDates(date1,date2);
-console.log("Difference in days: " + timeConversion[0] + ", in hours: " + timeConversion[1]);
-console.log(numberLib.strToNum("5678"));
 console.log(" ");
 
 // Array Tests
@@ -195,4 +129,5 @@ var arrayObjects = [{a:6},{a:1},{a:8},{a:2},{a:3},{a:5},{a:4},{a:7},{b:3},{b:5},
 console.log(arrayLib.sortKeyByValInArray(arrayObjects,"a"));
 var arrayList = ["Tim","Angel", 1050, 1050, 1050, "Steven",1050,30,10];
 console.log(arrayLib.dupInArray(1050,arrayList));
+
 
